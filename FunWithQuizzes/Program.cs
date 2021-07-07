4db18e8a6e89551e6266624164f9d31b112b1763
KristineGray <krisgraya@gmail.com>
2021-07-07 16:35:19 -0400

    initial commit

---
+++ FunWithQuizzes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FunWithQuizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Project should:
+                1. create several questions, 
+                2. present them to the user, 
+                3. accept the user’s responses, and then 
+                4. tell them whether their answers were correct or incorrect
+             */
+        }
+    }
+}
---
+++ TestQuizzes/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FunWithQuizzes;
+
+namespace TestQuizzes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EmptyTestMethod1()
+        {
+            Assert.IsTrue(5 == 5);
+        }
+    }
+}
